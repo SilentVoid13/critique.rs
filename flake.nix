d9cf5bc3057f1b76c9acd63bc9eb7e4a515dd94b
SilentVoid13 <51264226+SilentVoid13@users.noreply.github.com>
2024-02-12 17:53:09 +0100

    ci: adding flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "critique.rs";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs: let
+  in
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            rustc
+            openssl
+          ];
+        };
+      }
+    );
+}
