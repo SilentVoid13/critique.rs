f46f2b64be7e1b06ee4bd4b6a9724499299545d8
silentvoid13 <51264226+SilentVoid13@users.noreply.github.com>
2025-03-25 22:46:53 +0100

    refactor: improving media universe parsing

--- critique2boxd/src/args.rs
+++ critique2boxd/src/args.rs
@@ -1,17 +1,22 @@
 use clap::Parser;
 use std::path::PathBuf;
+use critique_api::MediaUniverse;
 
 #[derive(Debug, Parser)]
 pub struct CliArgs {
     /// SensCritique username
     pub username: String,
     /// Media type
-    #[arg(short, long, default_value = "film")]
-    pub media_type: String,
+    #[arg(short, long, default_value = "film", value_parser = parse_media_universe)]
+    pub media_type: MediaUniverse,
     /// Add reviews to the output
     #[arg(short, long, default_value_t = true)]
     pub with_reviews: bool,
     /// Output CSV file
     #[arg(short, long, default_value = "output.csv")]
     pub output: PathBuf,
+}
+
+fn parse_media_universe(s: &str) -> Result<MediaUniverse, String> {
+    MediaUniverse::try_from(s).map_err(|e| "Valid values: movie, book, game, tvShow, comicBook, musicAlbum, musicTrack".to_string())
 }
--- critique2boxd/src/main.rs
+++ critique2boxd/src/main.rs
@@ -27,27 +27,7 @@ fn main() -> Result<()> {
 
     println!("[*] Fetching user collection...");
 
-    let media_universe;
-    if args.media_type == "film" {
-        media_universe = MediaUniverse::Film;
-    } else if args.media_type == "tvShow" {
-        media_universe = MediaUniverse::TvShow;
-    } else if args.media_type == "game" {
-        media_universe = MediaUniverse::Videogame;
-    } else if args.media_type == "book" {
-        media_universe = MediaUniverse::Book;
-    } else if args.media_type == "comicBook" {
-        media_universe = MediaUniverse::ComicBook;
-    } else if args.media_type == "musicAlbum" {
-        media_universe = MediaUniverse::MusicAlbum;
-    } else if args.media_type == "musicTrack" {
-        media_universe = MediaUniverse::MusicTrack;
-    } else {
-        eprintln!("[-] Unsupported media type: {}", args.media_type);
-        std::process::exit(1);
-    }
-
-    let res = client.get_user_collection(&args.username, None, None, Some(media_universe))?;
+    let res = client.get_user_collection(&args.username, None, None, Some(args.media_type))?;
     let film_collection = res
         .user
         .ok_or_eyre("no user in response")?
